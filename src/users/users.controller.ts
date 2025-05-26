1007a346c0b91fbb278b978712fdbaac901d455a
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2025-05-26 12:50:05 -0300

    feat: adiciona rotas básicas do sistema e serialização de objeto de retorno a partir de interceptors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+SerializeInterceptor
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+    
+    @Post('/signup')
+    createUser(
+        @Body() body: CreateUserDto
+    ) {
+        return this.usersService.create(body.email, body.password)
+    }
+    
+    @Serialize(UserDto)
+    @Get(':id') 
+    async findUser(@Param("id") id: string) {
+        console.log("handler running")
+        const user = await this.usersService.findOne(parseInt(id));
+        if(!user) {
+            throw new NotFoundException("User not found");
+        }
+        return user
+    }
+
+    @Serialize(UserDto)
+    @Get() 
+    findAllUsers(@Query("email") email: string) {
+        return this.usersService.find(email);
+    }
+
+    @Delete('id')
+    removeUser(@Param("id") id: string) {
+        return this.usersService.remove(parseInt(id))
+    }
+
+    @Patch(':id')
+    updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), body)
+    }
+
+}
